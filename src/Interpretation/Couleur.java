257c204c3ed3a36f372fa9ace83e9bcb8ff3a696
thibaut <thibaut.depommier@gmail.com>
2017-09-21 16:45:42 +0200

    Ajout de l'enum pour les couleur

---
+++ src/Interpretation/Couleur.java
@@ -0,0 +1,37 @@
+package Interpretation;
+
+import javafx.scene.paint.Color;
+
+public enum Couleur {
+
+	ROUGE(Color.RED,"ROUGE"),
+	BLEU(Color.BLUE,"BLEU"),
+	BLANC(Color.WHITE,"BLANC"),
+	GRIS(Color.GREY,"GRIS"),
+	VERT(Color.GREEN,"VERT"),
+	ORANGE(Color.ORANGE,"ORANGE"),
+	JAUNE(Color.YELLOW,"JAUNE"),
+	ROSE(Color.PINK,"ROSE"),
+	VIOLET(Color.VIOLET,"VIOLET"),
+	MARRON(Color.MAROON,"MARRON");
+	
+	
+	
+	Color c;
+	String name;
+	
+	Couleur(Color c, String name){
+		this.c = c;
+		this.name = name;
+	}
+
+	public Color getColor() {
+		return c;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+}
