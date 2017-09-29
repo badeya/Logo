a27c22da92a9635686f74873d16168463a090872
Thibaut DEPOMMIER <thibaut.depommier@etudiant.univ-lille1.fr>
2017-09-29 09:12:59 +0200

    ajout d'une TODO dans le readme

---
+++ src/interpretation/Crayon.java
@@ -0,0 +1,77 @@
+package interpretation;
+
+import javafx.scene.paint.Color;
+
+public class Crayon {
+
+	private Coordonne coord = new Coordonne(0, 0);
+	private Color couleur = Color.BLACK;
+	private boolean ecrit = true;
+	private int orientation = 0;
+	private int width = 5;
+
+	
+	public Crayon(int x, int y) {
+		coord = new Coordonne(x, y);
+	}
+	
+	public void setWidth(int x){
+		this.width = x;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public Coordonne getCoord(){
+		return coord;
+	}
+	
+	public void setOrientation(int o){
+		this.orientation = o;
+	}
+	
+	public double getOrientation(){
+		return this.orientation;
+	}
+
+	public double getX() {
+		return coord.getX();
+	}
+
+
+	public void setX(double x) {
+		coord.setX(x);
+	}
+
+
+	public double getY() {
+		return coord.getY();
+	}
+
+
+	public void setY(double y) {
+		coord.setY(y);
+	}
+
+
+	public Color getCouleur() {
+		return couleur;
+	}
+
+
+	public void setCouleur(Color couleur) {
+		this.couleur = couleur;
+	}
+
+
+	public boolean isEcrit() {
+		return ecrit;
+	}
+
+	public void setEcrit(boolean ecrit) {
+		this.ecrit = ecrit;
+	}	
+	
+	
+}
